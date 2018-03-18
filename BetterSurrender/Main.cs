01fe0e9b1e71aa630efea05a86d1261016b857ee
Colby Gallup <letgomylegomon100@gmail.com>
2018-03-18 00:52:15 -0500

    Initial commit

---
+++ BetterSurrender/Main.cs
@@ -0,0 +1,39 @@
+﻿using Rocket.Core.Plugins;
+using Rocket.API;
+using Rocket.Unturned;
+using Rocket.Unturned.Player;
+
+namespace BetterSurrender
+{
+    public class Main : RocketPlugin
+    {
+        public static Main Instance;
+
+        protected override void Load()
+        {
+            base.Load();
+            Instance = this;
+
+            U.Events.OnPlayerConnected += (UnturnedPlayer player) =>
+            {
+                if (player.HasPermission("bettersurrender"))
+                {
+                    player.Events.OnUpdateGesture += PlayerEvents.GestureHandler;
+                }
+            };
+        }
+
+        protected override void Unload()
+        {
+            base.Unload();
+        }
+
+        private void FixedUpdate()
+        {
+            if (this.State != PluginState.Loaded)
+            {
+                return;
+            }
+        }
+    }
+}
---
+++ BetterSurrender/PlayerEvents.cs
@@ -0,0 +1,20 @@
+﻿using Rocket.Unturned.Player;
+using static Rocket.Unturned.Events.UnturnedPlayerEvents;
+
+namespace BetterSurrender
+{
+    class PlayerEvents
+    {
+        public static void GestureHandler(UnturnedPlayer player, PlayerGesture gesture)
+        {
+            if (gesture == PlayerGesture.SurrenderStart)
+            {
+                player.GodMode = true;
+            }
+            else
+            {
+                player.GodMode = false;
+            }
+        }
+    }
+}
